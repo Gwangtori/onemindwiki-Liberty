5e81b5e4f9dada2c4daa4f9725bdaac9579d08a6
광토리 <64156915+Gwangtori@users.noreply.github.com>
2023-11-08 14:25:06 +0900

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -14,39 +14,64 @@ function liberty_do_func_xss_encode(data) {
 //매개 변수 parent는 ul태그여야 합니다
 function ShowAjaxRecentList(parent) {
     function temp() {
-        jQuery.ajax({
-            url: "https://namgall.wikiing.in/api/open_recent_changes", // 수정된 API URL
-            dataType: 'json'
-        }).done(function (res) {
-            var html = "";
-            for (var i = 0; i < res.length && i < 10; i++) {
-                var item = res[i];
-                if (liberty_do_func_xss_encode(item[7]) == "소나") {
-                    html += '<li><a class="recent-item" href="https://sona.wiki/w/' + encodeURIComponent(item[1]) + '" title="' + liberty_do_func_xss_encode(item[1]) + '">';
-                } else if (liberty_do_func_xss_encode(item[7]) == "위브") {
-                    html += '<li><a class="recent-item" href="https://weve.wiki/w/' + encodeURIComponent(item[1]) + '" title="' + liberty_do_func_xss_encode(item[1]) + '">';
-                } else if (liberty_do_func_xss_encode(item[7]) == "시드") {
-                    html += '<li><a class="recent-item" href="https://seecl.wiki/w/' + encodeURIComponent(item[1]) + '" title="' + liberty_do_func_xss_encode(item[1]) + '">';
-                } else if (liberty_do_func_xss_encode(item[7]) == "다올") {
-                    html += '<li><a class="recent-item" href="https://wiki.daol.cc/w/' + encodeURIComponent(item[1]) + '" title="' + liberty_do_func_xss_encode(item[1]) + '">';
-                }
-                html += "[" + liberty_do_func_xss_encode(item[2].replace(/^([^ ]+) /, '')) + "] " + "[" + liberty_do_func_xss_encode(item[7]) + "] ";
-                var text = item[1];
-                if (text.length > 18) {
-                    text = text.substr(0, 18);
-                    text += "...";
-                }
-                html += text;
-                html += "</a></li>";
-            }
+	if(liberty_load_type === 'A') {
+	        jQuery.ajax({
+	            url: "https://namgall.wikiing.in/api/open_recent_changes", // 수정된 API URL
+	            dataType: 'json'
+	        }).done(function (res) {
+	            var html = "";
+	            for (var i = 0; i < res.length && i < 10; i++) {
+	                var item = res[i];
+	                if (liberty_do_func_xss_encode(item[7]) == "소나") {
+	                    html += '<li><a class="recent-item" href="https://sona.wiki/w/' + encodeURIComponent(item[1]) + '" title="' + liberty_do_func_xss_encode(item[1]) + '">';
+	                } else if (liberty_do_func_xss_encode(item[7]) == "위브") {
+	                    html += '<li><a class="recent-item" href="https://weve.wiki/w/' + encodeURIComponent(item[1]) + '" title="' + liberty_do_func_xss_encode(item[1]) + '">';
+	                } else if (liberty_do_func_xss_encode(item[7]) == "시드") {
+	                    html += '<li><a class="recent-item" href="https://seecl.wiki/w/' + encodeURIComponent(item[1]) + '" title="' + liberty_do_func_xss_encode(item[1]) + '">';
+	                } else if (liberty_do_func_xss_encode(item[7]) == "다올") {
+	                    html += '<li><a class="recent-item" href="https://wiki.daol.cc/w/' + encodeURIComponent(item[1]) + '" title="' + liberty_do_func_xss_encode(item[1]) + '">';
+	                }
+	                html += "[" + liberty_do_func_xss_encode(item[2].replace(/^([^ ]+) /, '')) + "] " + "[" + liberty_do_func_xss_encode(item[7]) + "] ";
+	                var text = item[1];
+	                if (text.length > 18) {
+	                    text = text.substr(0, 18);
+	                    text += "...";
+	                }
+	                html += text;
+	                html += "</a></li>";
+	            }
+	                
+	            if(parent != null) {
+	                jQuery(parent).html(html);
+	            }
+	        });
+        } else {
+            jQuery.ajax({
+                url: "/api/recent_discuss/10", // 호출 URL
+                dataType:'json'
+            }).done(function(res) {
+                let html = "";
+                for(let i = 0; i < res.length && i < 10; i++) {
+                    let item = res[i];
 
-            if (parent != null) {
-                jQuery(parent).html(html);
-            }
-        });
-    }
-
-    temp();
+                    html += '<li><a class="recent-item" href="/thread/' + encodeURIComponent(item[3]) + '" title="' + liberty_do_func_xss_encode(item[1]) +'">';
+                    html += "[" + item[2].replace(/^([^ ]+) /, '') + "] ";
+                    let text = item[1];
+                    if(text.length > 13) {
+                        text = text.substr(0, 13);
+                        text +="...";
+                    }
+                    html += liberty_do_func_xss_encode(text);
+                    html += "</a></li>";
+                }
+                
+                if(parent != null) {
+                    jQuery(parent).html(html);
+                }
+            });
+        }
+	}
+	temp();
 }
 
 function liberty_side_load(get_type) {
